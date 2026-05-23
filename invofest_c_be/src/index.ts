885985738e67043530533b302b2a42037b66501c
alifavicena <avisganteng134@gmail.com>
2026-05-23 13:51:10 +0700

    update backend

--- invofest_c_be/src/index.ts
+++ invofest_c_be/src/index.ts
@@ -1,27 +1,28 @@
-import express from "express";
-import cors from "cors";
+    import express from "express";
+    import cors from "cors";
 
-import eventRoutes from "./routes/eventRoute.js";
-import categoryRoutes from "./routes/categoryRoute.js";
-import pembicaraRoutes from "./routes/pembicaraRoute.js";
-import dashboardRoutes from "./routes/dashboardRoute.js";
+    import eventRoutes from "./routes/eventRoute.js";
+    import categoryRoutes from "./routes/categoryRoute.js";
+    import pembicaraRoutes from "./routes/pembicaraRoute.js";
+    import dashboardRoutes from "./routes/dashboardRoute.js";
 
-const app = express();
-const port = 3000;
+    const app = express();
+    const port = 3000;
 
-app.use(cors());
-app.use(express.json());
+    app.use(cors());
+    app.use(express.json());
 
-app.get("/", (req, res) => {
-    res.send("Ini adalah api untuk invofest");
-});
+    app.get("/", (req, res) => {
+        res.send("Ini adalah api untuk invofest");
+    });
 
-app.use("/events", eventRoutes);
-app.use("/categories", categoryRoutes);
-app.use("/pembicara", pembicaraRoutes);
-app.use("/dashboard", dashboardRoutes);
+    app.use("/events", eventRoutes);
+    app.use("/categories", categoryRoutes);
+    app.use("/pembicara", pembicaraRoutes);
+    app.use("/dashboard", dashboardRoutes);
 
-app.listen(port, () => {
-    console.log(`Server is running on http://localhost:${port}`);
-});
+    app.listen(port, () => {
+        console.log(`Server is running on http://localhost:${port}`);
+    });
 
+export default app;
